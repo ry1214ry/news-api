7c4a2e7dc0dbf526db27ced2178518840acc5d31
RuneMari <your_email@example.com>
2026-03-04 17:01:45 +0700

    Update model, controller, and routes for API

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,11 +11,6 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,3 @@ use App\Http\Controllers\Api\SlideshowController;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
-Route::apiResource('news', NewsController::class);
-Route::apiResource('categories', CategoryController::class);
-Route::apiResource('slideshows', SlideshowController::class);
